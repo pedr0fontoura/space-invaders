55b5f4f3241dd29c14a338ee16a98e23e7bec992
SnakeWiz <pffrdias@gmail.com>
2021-04-08 18:42:41 -0300

    tweak(vendor): move PPlay to a submodule

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from PPlay.window import *
+import os
+import sys
+
+# Add vendor directory to module search path
+parent_dir = os.path.abspath(os.path.dirname(__file__))
+vendor_dir = os.path.join(parent_dir, 'vendor')
+
+pplay_dir = os.path.join(vendor_dir, 'pplay')
+
+sys.path.append(vendor_dir)
+
+# Handle PPlay repo structure
+sys.path.append(pplay_dir)
+
 from game import Game
 
 game = Game()
